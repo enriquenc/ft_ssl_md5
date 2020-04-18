caf9d02b559cb6858c553e6a42a784fa7106268c
enriquenc <tarasmaslianko@gmail.com>
2020-04-18 22:30:21 +0300

    [Tests] Add trial test using unittest lib.

--- test/test.py
+++ test/test.py
@@ -1,53 +1,28 @@
-import hashlib
+import unittest
 import os
-
 import subprocess
+import hashlib
 
-def md5_test(text):
-	org = hashlib.md5(text.encode('utf-8')).hexdigest()
-	result = subprocess.run(['./ft_ssl', 'md5', '-s', text], stdout=subprocess.PIPE)
-	my = result.stdout.decode('utf-8').rstrip()
-	if org == my:
-		print("OK")
-	else:
-		print("ERROR")
-		print("org: " + org)
-		print("my: " + my)
+def test_preparation():
+	if ('/' not in __file__):
+		os.chdir("../")
+	subprocess.run(["make"], stdout=subprocess.PIPE)
 
+def clean_after_test():
+	subprocess.run(["make", "fclean"], stdout=subprocess.PIPE)
 
-def hash_test(text):
-	print("MD5 algorithm checker: ", end='')
-	md5_test(text)
+class TestAll(unittest.TestCase):
+	def test_test(self):
+		stdin_data = "123"
+		s_flag_data = "fuck"
+		stdin_encoded = hashlib.md5(stdin_data.encode('utf-8')).hexdigest()
+		s_data_encoded = hashlib.md5(s_flag_data.encode('utf-8')).hexdigest()
+		expected = stdin_data + stdin_encoded + "\nmd5 (\"" + s_flag_data + "\") = " + s_data_encoded + "\n"
+		actual = subprocess.Popen(['./ft_ssl', 'md5', '-s', s_flag_data, '-p'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+		actual = actual.communicate(b'123')[0].decode('utf-8')
+		self.assertEqual(expected, actual)
 
-def copy_binary_for_test():
-	subprocess.run(['cp', "../ft_ssl", "."], stdout=subprocess.PIPE)
-
-copy_binary_for_test()
-hash_test('')
-hash_test('1')
-hash_test('123r4rurjiro3r4tgh')
-hash_test('yihurio0783903459884930495849034958thugjikjbfekdofijdskodskjofjkjfbhvjfljkfkjlfkjjgfjk')
-hash_test('#pragma once\
-#include <my_openssl.h>\
-\
-# define F(B, C, D) 			((B) & (C)) | (~(B) & (D))\
-# define G(B, C, D) 			((B) & (D)) | ((C) & ~(D))\
-# define H(B, C, D) 			(B) ^ (C) ^ (D)\
-# define I(B, C, D) 			(C) ^ (B | ~(D))\
-\
-#define LEFT_ROTATE(x, c) ((x << c) | (x >> (32 - c)));\
-\
-#define NEEDED_MODULO_BYTES (448 / 8)\
-#define DIV_BYTES (512 / 8)\
-\
-#define CHUNK_LEN_BYTES (512 / 8)\
-\
-#define A_INIT_VALUE 0x67452301; // 67452301h\
-#define B_INIT_VALUE 0xefcdab89; // EFCDAB89h\
-#define C_INIT_VALUE 0x98badcfe; // 98BADCFEh\
-#define D_INIT_VALUE 0x10325476; // 10325476h\
-')
-
-with open('war_and_peace', 'r') as file:
-	data = file.read().replace('\n', '')
-	hash_test(data[:1024])
+if __name__ == '__main__':
+	test_preparation()
+	unittest.main()
+	clean_after_test()
